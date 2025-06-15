db58e256d066dacc57e5ee4dc143922dbb36d481
KarthikeyanV2K <karthivv591@gmail.com>
2025-06-15 13:18:55 +0530

    fear: switching to localhost to connectus,net,in

--- Server.js
+++ Server.js
@@ -23,7 +23,7 @@ const app = express();
 app.use(express.json());
 
 app.use(cors({
-  origin: 'http://localhost:3000',
+  origin: 'https://connectus.net.in',
   methods: ['GET', 'POST', 'PUT', 'DELETE'],
   credentials: true
 }));
